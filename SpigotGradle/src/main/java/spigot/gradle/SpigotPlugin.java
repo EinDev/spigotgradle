849919d6dab7f087a07535a0f94dc5e3d1ad12eb
Firestar99 <31222740+firestar99@users.noreply.github.com>
2020-05-08 00:38:46 +0300

    initial commit

---
+++ SpigotGradle/src/main/java/spigot/gradle/SpigotPlugin.java
@@ -0,0 +1,13 @@
+package spigot.gradle;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+@NonNullApi
+public class SpigotPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+    }
+}
